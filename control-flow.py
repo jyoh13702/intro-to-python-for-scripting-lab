80d43ff7cf0ba4b762e721b974ad531a08bf3ed5
jyoh13702 <jyoh13702@gmail.com>
2025-12-17 22:47:47 -0500

    voting

--- control-flow.py
+++ control-flow.py
@@ -1,23 +1,3 @@
-# Problem 1: Vowel or consonant
-#
-# Write a Python function named `check_letter` that determines if a given letter
-# is a vowel or a consonant.
-#
-# Requirements:
-# - The function should prompt the user to enter a letter (a-z or A-Z) and 
-#   determine its type.
-# - It should handle both uppercase and lowercase letters.
-# - If the letter is a vowel (a, e, i, o, u), print: "The letter x is a vowel."
-# - If the letter is a consonant, print: "The letter x is a consonant."
-# - Replace 'x' with the actual letter entered by the user in the above
-#   messages.
-#
-# Hints:
-# - Use the `input()` function to capture user input.
-# - Utilize the `in` operator to check for vowels. You may need to look up
-#   how to use this online.
-# - Ensure your code provides feedback for non-alphabetical or invalid entries.
-
 def check_letter():
     letter = input("Enter a letter (a-z or A-Z): ")
     if not letter.isalpha() or len(letter) != 1:
@@ -31,3 +11,20 @@ def check_letter():
         print(f"The letter {letter} is a consonant.")
 # Call the function
 check_letter()
+
+
+def check_voting_eligibility():
+    Voting_Age = 18
+    Age = int(input("Please enter your age: "))
+    if Age < 0:
+        print("Age cannot be negative.")
+    elif Age >= Voting_Age:
+        print("You are eligible to vote.")
+    elif Age < Voting_Age:
+        print("You are not eligible to vote.")
+    else:
+        print("Input a valid age.")
+    
+
+# Call the function
+check_voting_eligibility()
